decc4ef87ca7dcbecde9b9b9230abd52e74ff9d3
Peter Spicer <peter.spicer@catalyst-eu.net>
2017-10-05 09:55:51 +0100

    Bug fixes for the plugin.
    The following WRs are covered:
    WR280787 - inconsistency of labels in editing settings
    WR280799 - question response after submission
    WR280800 - image position after question response
    WR280803 - view all responses page, formatting and icon

--- type/poll/classes/viewall.php
+++ type/poll/classes/viewall.php
@@ -65,7 +65,7 @@ class viewall extends abstractoutput implements renderable, templatable {
         $data->all_responses = !empty($this->data->all_responses) ? $this->data->all_responses : array();
         $data->group_selector = !empty($this->data->group_selector) ? $this->data->group_selector : '';
 
-        $data->icon = $OUTPUT->render(new pix_icon('icon', '', 'responsetype_text'));
+        $data->icon = $OUTPUT->render(new pix_icon('icon', '', 'responsetype_poll'));
 
         $data->responsetype = $this->data->responsetype;
         // Combine the answer possibilities into the answers from users.
