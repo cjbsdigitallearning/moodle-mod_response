bf3bf2b619b6471cdcd4f037fabd2ad1eeb2a9d4
Peter Spicer <peter.spicer@catalyst-eu.net>
2021-03-23 18:20:30 +0000

    WR302477 Move deletion of parent record to after subplugins
    This allows subplugins to reference the parent row for lookups etc.
    prior to any deletion they might do themselves.
    
    Change-Id: I3e37bb4efa1ac0d704ee066ead983f9aca841671

--- lib.php
+++ lib.php
@@ -153,10 +153,11 @@ function response_delete_instance($id) {
     global $DB;
     // Before we delete it, we need to know what kind of response it was.
     $response = $DB->get_record('response', array('id' => $id));
-    $DB->delete_records('response', array('id' => $id));
 
     $subplugin = helper::instance_factory($response->responsetype, 'configuration');
     $subplugin->delete_instance($id);
+
+    $DB->delete_records('response', array('id' => $id));
 
     return true;
 }
