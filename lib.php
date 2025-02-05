30f096b962330716c54bd47b09e7c304e50e0ec0
Michael Kotlyar <michael.kotlyar@catalyst-eu.net>
2025-02-05 09:24:41 +0000

    WR449772: Implement hook to display on timelines.

--- lib.php
+++ lib.php
@@ -685,3 +685,39 @@ function mod_response_output_fragment_completion($args) {
 
     return $output;
 }
+
+/**
+ * This function receives a calendar event and returns the action associated with it, or null if there is none.
+ *
+ * This is used by block_myoverview in order to display the event appropriately. If null is returned then the event
+ * is not displayed on the block.
+ *
+ * @param calendar_event $event
+ * @param \core_calendar\action_factory $factory
+ * @return \core_calendar\local\event\entities\action_interface|null
+ */
+function mod_response_core_calendar_provide_event_action(calendar_event $event,
+                                                      \core_calendar\action_factory $factory, $userid = 0) {
+    global $USER;
+
+    if (empty($userid)) {
+        $userid = $USER->id;
+    }
+
+    $cm = get_fast_modinfo($event->courseid, $userid)->instances['response'][$event->instance];
+
+    $completion = new \completion_info($cm->get_course());
+
+    $completiondata = $completion->get_data($cm, false, $userid);
+
+    if ($completiondata->completionstate != COMPLETION_INCOMPLETE) {
+        return null;
+    }
+
+    return $factory->create_instance(
+        get_string('view'),
+        new \moodle_url('/mod/response/view.php', ['id' => $cm->id]),
+        1,
+        true
+    );
+}
