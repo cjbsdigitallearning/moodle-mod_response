844f1cfe119303b0fd794ed7ad67560cb620ef9b
Michael Kotlyar <michael.kotlyar@catalyst-eu.net>
2025-05-21 15:16:01 +0100

    WR460609: Support default feature completion.

--- lib.php
+++ lib.php
@@ -42,6 +42,7 @@ function response_supports($feature) {
         case FEATURE_GROUPS:
         case FEATURE_COMPLETION:
         case FEATURE_COMPLETION_HAS_RULES:
+        case FEATURE_MODEDIT_DEFAULT_COMPLETION:
         case FEATURE_BACKUP_MOODLE2:
             return true;
         default:
